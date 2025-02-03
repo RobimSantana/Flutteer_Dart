fbfc67506ec32ad5328b83a2a44c8ccab5004e3d
Robson Santana <www.robson.dev@gmail.com>
2025-02-03 08:11:06 -0300

    Primeiro commit: Estudando Dart

---
+++ bin/meu_app.dart
@@ -0,0 +1,10 @@
+void main(List<String> arguments) {
+  double numero1 = 10.1;
+  var numero2 = 11.1;
+
+  print ( "Remove ponto flutuante");
+  print(numero1.truncate());
+  
+
+}
+
---
+++ lib/meu_app.dart
@@ -0,0 +1,3 @@
+int calculate() {
+  return 20 * 7;
+}
